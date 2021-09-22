788c16e40ff879f7d015b3aef317f6a9dd1db6b9
devemmm <djntivuguruzwaemmanuel@gmail.com>
2021-09-22 03:18:13 +0200

    initial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,15 @@
 const moongose = require("mongoose");
-
 moongose
     .connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false
     })
     .then(() => {
         return console.log("Connected To Database");
     })
     .catch((error) => {
+        console.log(error)
         return console.log("Unable to Connect To Database try Again");
     });
